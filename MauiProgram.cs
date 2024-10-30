d5b0acad5760102c675941416264bbaf67e8cf4e
Dillon-Duncan <mrduncan.business@gmail.com>
2024-10-30 06:02:39 +0200

    Initial Commit

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using YMCA3.Components.Pages;
+using YmcaApiClient.IoC;
 
 namespace YMCA3
 {
@@ -13,6 +15,18 @@ namespace YMCA3
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
+
+            builder.Services.AddYmcaApiClientService(x => x.ApiBaseAddress = "http://localhost:5013");
+            builder.Services.AddTransient<Admin>();
+            builder.Services.AddTransient<BulletinBoard>();
+            builder.Services.AddTransient<Calendar>();
+            builder.Services.AddTransient<Chats>();
+            builder.Services.AddTransient<Chatting>();
+            builder.Services.AddTransient<Login>();
+            builder.Services.AddTransient<News>();
+            builder.Services.AddTransient<Register>();
+            builder.Services.AddTransient<UserAccount>();
+            builder.Services.AddTransient<Volunteer>();
 
             builder.Services.AddMauiBlazorWebView();
 
